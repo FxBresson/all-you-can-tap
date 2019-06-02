c741ca6c9ae5f6f60ba3d3dd11d48b98fb18bda5
FxBresson <fx-bresson@live.fr>
2019-06-02 16:06:11 +0200

    @Add: APP - Game logic

--- ANGclient/src/app/routes/user-page/user-page.component.ts
+++ ANGclient/src/app/routes/user-page/user-page.component.ts
@@ -8,35 +8,75 @@ Imports & definition
   import { IdentityModel } from '../../models/identity.model';
   import { ApiResponseModel } from "../../models/api.reponse.model";
   import { AuthService } from "../../services/auth/auth-service.service";
+  import { Router } from '@angular/router';
+  import { RequesterService } from '../../services/requester/requester.service';
 
   // Definition
   @Component({
     selector: 'app-user-page',
     templateUrl: './user-page.component.html',
-    providers: [ AuthService ]
+    styleUrls: ['./user-page.component.scss'],
+    providers: [ AuthService, RequesterService ]
   })
 //
-
 
 /* 
 Export
 */
   export class UserPageComponent implements OnInit {
 
+    public score: number = 0;
+    private gameRunning: boolean = false;
+    public progress: number = 0;
+    private interval: NodeJS.Timer = null;
+    private precision: number = 10;
+    private gameDuration: number = 10;
+
     /* 
     Config.
     */
       // Module injection
       constructor(
-        private AuthService: AuthService
+        private AuthService: AuthService,
+        private router: Router,
+        private requester: RequesterService
       ) {};
     //
-
 
     /*
     Methods
     */
     //
+    public tap() {
+      this.score++;
+
+      if(!this.gameRunning) {
+        this.startGame()
+      }
+    }
+
+    private startGame() {
+      this.gameRunning = true;
+      this.interval = setInterval(() => {
+        this.progress += (100/this.gameDuration)/this.precision
+        if (this.progress >= 100 && this.gameRunning) {
+          this.stopGame()
+        }
+      }, (1000/this.precision))
+    }
+
+    private stopGame() {
+      this.gameRunning = false;
+      clearInterval(this.interval)
+      this.interval = null;
+      this.progress = 0;
+
+      this.requester.post('/game', {
+        score: this.score
+      }).then(apiResponse => {
+        this.router.navigate(['tap'])
+      }).catch(alert)
+    }
 
 
     /* 
